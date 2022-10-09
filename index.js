31e6d2d8d959985b8e480e99f73120b379a3eb74
DewaBiara <angabiara@gmail.com>
2022-10-09 22:43:23 +0800

    fix error

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.get('/cars/:id', middleware.setCar, handler.handleGetCar);
 app.get('/cars/:id/update', middleware.setCar, handler.handlePageUpdateCar);
 app.post('/cars/:id/update', middleware.setCar, handler.handleupdateCar);
 app.get('/cars/:id/delete', middleware.setCar, handler.handleDeleteCar);
-app.put("/cars/:id/picture",
 
 app.listen(PORT, () => {
     console.log("server berjalan!")
