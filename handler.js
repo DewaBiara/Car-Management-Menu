4b35a956c92a67303b8ee42f8313e13ed6b548d5
DewaBiara <angabiara@gmail.com>
2022-10-09 22:17:19 +0800

    update project

---
+++ handler.js
@@ -0,0 +1,108 @@
+const { Cars } = require('./models');
+
+function handleRoot(req, res){
+  Cars.findAll().then(Cars => {
+    res.status(200).render("Cars/index", {
+      Cars: Cars,
+    });
+  }).catch((err) => {
+    res.status(400).json({
+      status: "FAIL",
+      message: err.message,
+    });
+  });
+}
+
+function handlePageCreateCar(req, res){
+  res.render("cars/create");
+}
+
+function handleCreateCar(req, res){
+    Cars.create({
+        name : req.body.name,
+        type : req.body.type,
+        price : req.body.price,
+        image : req.body.image,
+        size : req.body.size
+    }).then(Cars => {
+        res.redirect(201, "/cars/");     
+    }).catch((err) => {
+        res.status(400).json({
+          status: "FAIL",
+          message: err.message,
+        });
+      });
+}
+
+function handleCar(req, res){
+  Cars.findAll().then(Cars => {
+      res.status(200).render("Cars/index", {
+        Cars: Cars,
+      });
+  }).catch((err) => {
+      res.status(400).json({
+        status: "FAIL",
+        message: err.message,
+      });
+    });
+}
+
+function handleGetCar(req, res){
+    const car = req.car;
+    res.render("cars/_id/index", {
+      id: car.id,
+      name: car.name,
+      type: car.type,
+      price: car.price,
+      image: car.image,
+      size: car.size,
+      updateAt: car.updateAt
+    });
+}
+
+function handlePageUpdateCar(req, res){
+  const car = req.car;
+  res.render("cars/_id/update", {
+    id: car.id,
+    name: car.name,
+    type: car.type,
+    price: car.price,
+    image: car.image,
+    size: car.size
+  });
+}
+
+function handleupdateCar(req, res){
+    const car = req.car;
+    car.update(req.body).then(()=>{
+      res.redirect(201, "/cars/"); 
+    }).catch((err) => {
+        res.status(400).json({
+          status: "FAIL",
+          message: err.message,
+        });
+      });
+}
+
+function handleDeleteCar(req, res){
+  const car = req.car;
+  car.destroy().then(()=>{
+      res.redirect(204, "/cars");
+  }).catch((err) => {
+      res.status(400).json({
+        status: "FAIL",
+        message: err.message,
+      });
+    });
+}
+
+module.exports = {
+    handleRoot,
+    handleCreateCar,
+    handleCar,
+    handleGetCar,
+    handleDeleteCar,
+    handleupdateCar,
+    handlePageCreateCar,
+    handlePageUpdateCar
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const handler = require('./handler');
+const middleware = require('./middleware');
+const upload = require("./upload");
+const path = require("path");
+
+const PORT = 8000;
+
+const PUBLIC_DIRECTORY = path.join(__dirname, "public");
+
+// Set format request
+app.use(express.urlencoded({ extended: true }));
+
+// Set PUBLIC_DIRECTORY sebagai static files di express
+app.use(express.static(PUBLIC_DIRECTORY));
+app.use(express.static(path.join(__dirname, "views")));
+
+// Bilang ke express kalo kita mau pake EJS sebagai view engine
+app.set("view engine", "ejs");
+
+app.use(express.json())
+
+app.get('/', handler.handleRoot);
+app.get('/cars', handler.handleCar);
+app.get('/cars/create', handler.handlePageCreateCar);
+app.post('/cars', handler.handleCreateCar);
+app.get('/cars/:id', middleware.setCar, handler.handleGetCar);
+app.get('/cars/:id/update', middleware.setCar, handler.handlePageUpdateCar);
+app.post('/cars/:id/update', middleware.setCar, handler.handleupdateCar);
+app.get('/cars/:id/delete', middleware.setCar, handler.handleDeleteCar);
+app.put("/cars/:id/picture",
+    upload.single("picture"),
+    (req, res) => {
+      const url = `/uploads/${req.file.filename}`;
+      res
+        .status(200)
+        .json({ message: "Foto berhasil di-upload, silahkan cek URL", url });
+    }
+  );
+  
+
+app.listen(PORT, () => {
+    console.log("server berjalan!")
+});
